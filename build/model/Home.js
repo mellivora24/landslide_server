182ee564c4f8f29b7e5a6c5b7461ed0332920550
Quyet_Thanh <115696569+thanhquyet24ptit@users.noreply.github.com>
2023-12-20 14:03:32 +0700

    Create Home.js

---
+++ build/model/Home.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var mongoose = require("mongoose");
+var Schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  kinh: {
+    type: String,
+    required: true
+  },
+  viDo: {
+    type: String,
+    required: true
+  },
+  doAm: {
+    type: String,
+    required: true
+  },
+  doAm1: {
+    type: String,
+    required: true
+  },
+  luongMua: {
+    type: String,
+    required: true
+  },
+  satLo: {
+    type: String,
+    required: true
+  },
+  thoiGianMua: {
+    type: String,
+    required: true
+  },
+  doNghieng: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+module.exports = mongoose.model("Home", Schema);
